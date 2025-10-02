8cd9e9ac1fad17edf75dc8f7a5b88018535defd3
Mayuresh Mhatre <devopmayu18@gmail.com>
2025-10-02 22:29:43 +0530

    Mail sending functionality completed using nodemailer by mailtrap

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,5 +1,48 @@
 import { text } from "express";
 import Mailgen from "mailgen";
+import nodemailer from 'nodemailer'
+import 'dotenv/config'
+
+
+const sendEmail = async (options) => {
+    const mailGenerator = new Mailgen({
+        theme: "default",
+        product: {
+            name: "Task Manager",
+            link: "https:taskmanagerlink.com"
+        }
+    })
+
+    const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent)
+    const emailHtml = mailGenerator.generate(options.mailgenContent)
+
+
+    // Looking to send emails in production? Check out our Email API/SMTP product!
+    const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: MAILTRAP_SMTP_PORT,
+    auth: {
+        user: MAILTRAP_SMTP_USER,
+        pass: MAILTRAP_SMTP_PASS
+    }
+    });
+
+    const mail = {
+        from: "taskmanager@example.com",
+        to: options.email,
+        subject: options.subject,
+        text: emailTextual,
+        html: emailHtml
+    }
+
+    try {
+        await transporter.sendMail(mail)
+    } catch (error) {
+        console.error("Email service failed silently. Verify your credentials")
+        console.error("Error: ",error)
+    }
+
+}
 
 const emailVerificationMailgenContent = (username, verificationUrl) => {
     return {
@@ -40,5 +83,6 @@ const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
 
 export {
     emailVerificationMailgenContent,
-    forgotPasswordMailgenContent
+    forgotPasswordMailgenContent,
+    sendEmail
 }
