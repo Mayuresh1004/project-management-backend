3993c84eeeb53692934faef64aef79a32f32e0b9
Mayuresh Mhatre <devopmayu18@gmail.com>
2025-10-03 21:25:42 +0530

    added validation functionality using express-validator for user registration

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { resgisterUser } from "../controllers/auth.controllers.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { userRegistrationValidator } from "../validators/index.js";
+
 
 const router = Router()
 
-router.route("/register").post(resgisterUser)
+router.route("/register").post(userRegistrationValidator(),validate, resgisterUser)
 
 
 export default router
---
+++ src/validators/index.js
@@ -0,0 +1,12 @@
+import { body } from "express-validator";
+
+const userRegistrationValidator = () => {
+    return [
+        body("email").trim().notEmpty().withMessage("Email is required").isEmail().withMessage("Email is invalid"),
+        body("username").trim().notEmpty().withMessage("Username is reqiured").isLength({min: 3}).withMessage("Username must be atleast 3 characters"),
+        body("password").trim().notEmpty().withMessage("Password is required"),
+        body("fullName").optional().trim()
+    ]
+}
+
+export { userRegistrationValidator }
